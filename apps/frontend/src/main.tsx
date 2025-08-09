82538dd3b7da71f11d73f0f30ad03de1607f1306
roukodes <roukodes@gmail.com>
2025-08-09 13:39:17 +0300

    feat: installing MUI

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,4 +1,8 @@
 import '@/index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
